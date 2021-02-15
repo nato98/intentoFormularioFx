249677ba9cf1d2883c9a7ee97c1daf0c60ec2e6b
Natalia Orozco <natalia.orozco@sitis.com.co>
2021-02-15 10:37:20 -0500

    initial commit

---
+++ apps/formulario1/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'formulario1-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.styl'],
+})
+export class AppComponent {
+  title = 'formulario1';
+}
